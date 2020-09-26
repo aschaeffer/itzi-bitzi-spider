463526c17d4262cf74ca5faaa44f73dd53fde3a4
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2020-09-26 15:24:56 +0200

    Fixed switch service

--- set_all_switch_off.py
+++ set_all_switch_off.py
@@ -1,6 +1,6 @@
-from .switch_service import SwitchService
+from switch_service import SwitchService
 
 if __name__ == '__main__':
     switch_service = SwitchService()
     switch_service.setup_switches()
-    switch_service.set_all_switch_off()
+    switch_service.set_all_switches_off()
