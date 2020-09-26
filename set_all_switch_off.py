00bfd17488f07226e9f0524884d5a8f886549997
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2020-09-26 15:22:26 +0200

    Added switch service

--- set_all_switch_off.py
+++ set_all_switch_off.py
@@ -1,5 +1,6 @@
-from server.switch import switchSetup, set_all_switch_off
+from .switch_service import SwitchService
 
 if __name__ == '__main__':
-    switchSetup()
-    set_all_switch_off()
+    switch_service = SwitchService()
+    switch_service.setup_switches()
+    switch_service.set_all_switch_off()
---
+++ switch_service.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+
+class SwitchService:
+
+    switch_ids = [ 1, 2, 3 ]
+    gpio_pins = [ 5, 6, 13 ]
+
+    def __init__(self):
+        self.setup_switches()
+
+    def get_gpio_pin(self, switch_id):
+        if 1 <= switch_id <= len(self.gpio_pins):
+            return self.gpio_pins[switch_id]
+        else:
+            raise ValueError("Illegal switch id: " + switch_id)
+
+    def get_switch_id(self, gpio_pin):
+        return self.gpio_pins.index(gpio_pin) + 1
+
+    def setup_switches(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        for gpio_pin in self.gpio_pins:
+            GPIO.setup(gpio_pin, GPIO.OUT)
+
+    def set_switch_state(self, gpio_pin, state):
+        if state == 1:
+            GPIO.output(gpio_pin, GPIO.HIGH)
+        elif state == 0:
+            GPIO.output(gpio_pin,GPIO.LOW)
+        else:
+            pass
+
+    def set_switch(self, switch_id, state):
+        self.set_switch_state(self.get_gpio_pin(switch_id), state)
+
+    def set_all_switches(self, state):
+        self.set_switch_state()
+        for switch_id in range(len(self.gpio_pins)):
+            self.set_switch(switch_id, state)
+
+    def set_all_switches_off(self):
+        self.set_all_switches(0)
+
+    def set_all_switches_on(self):
+        self.set_all_switches(1)
+
+if __name__ == '__main__':
+    switch_service = SwitchService()
